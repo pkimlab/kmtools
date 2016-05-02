c3d516a368d12da3311d7fc2d8be4fa773f1fcb7
Alexey Strokach <alex.strokach@utoronto.ca>
2016-05-02 19:15:56 -0400

    Load setup.py info from conda recipe.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
+from conda_build.metadata import MetaData
+
+META = MetaData('devtools/conda-recipe')
 
 setup(
-    name='ascommon',
-    version="0.0.1",
+    name=META.get_value('package/name'),
+    version=META.get_value('package/version'),
+    description=META.get_value('about/summary'),
+    url=META.get_value('about/home'),
+    author='Alexey Strokach',
+    author_email='alex.strokach@utoronto.ca',
     packages=['ascommon'],
     # setup_requires=['pytest-runner'],
     # tests_require=['pytest'],
