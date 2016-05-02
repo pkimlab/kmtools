dd2809e1e0485b8617461d2ccbba46f69cce1862
Alexey Strokach <alex.strokach@utoronto.ca>
2016-05-02 01:36:47 -0400

    Add dotfiles

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Common
 
-Helper functions and classes that are used by many different scripts.
+![Travis-CI](https://travis-ci.com/ostrokach/ascommon.svg?token=BMWgXWYypxhMyyCmTG1t)
 
+Helper functions and classes that are used by many different scripts.
--- ascommon/pdb_tools.py
+++ ascommon/pdb_tools.py
@@ -6,17 +6,35 @@ import re
 import logging
 from functools import lru_cache
 from lxml import etree
-
 import pandas as pd
-
-from elaspic.structure_tools import AAA_DICT
-
 from .system_tools import download
 
 
 logger = logging.getLogger(__name__)
 
 RE_TAG = re.compile('({.+})?(.+)')
+
+A_DICT = {
+    'A': 'ALA', 'R': 'ARG', 'N': 'ASN', 'D': 'ASP', 'C': 'CYS', 'E': 'GLU',
+    'Q': 'GLN', 'G': 'GLY', 'H': 'HIS', 'I': 'ILE', 'L': 'LEU', 'K': 'LYS',
+    'M': 'MET', 'F': 'PHE', 'P': 'PRO', 'S': 'SER', 'T': 'THR', 'W': 'TRP',
+    'Y': 'TYR', 'V': 'VAL', 'U': 'SEC', 'O': 'PYL',
+    'B': 'ASX', 'Z': 'GLX', 'J': 'XLE', 'X': 'XAA', '*': 'TER'
+}
+AAA_DICT = dict([(value, key) for key, value in list(A_DICT.items())])
+AAA_DICT['UNK'] = 'X'
+AAA_DICT['MSE'] = 'M'
+AAA_DICT['CSD'] = 'C'
+
+# Phosphorylated residues
+AAA_DICT['SEP'] = 'S'  # PHOSPHOSERINE
+AAA_DICT['TPO'] = 'T'  # PHOSPHOTHREONINE
+AAA_DICT['SEP'] = 'Y'  # O-PHOSPHOTYROSINE
+
+# Methylated lysines
+AAA_DICT['MLZ'] = 'K'
+AAA_DICT['MLY'] = 'K'
+AAA_DICT['M3L'] = 'K'
 
 
 class SIFTSError(Exception):
