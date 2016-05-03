8e1b5ce0bd7cf3e11e78899c2076992cc18598cf
Alexey Strokach <alex.strokach@utoronto.ca>
2016-05-02 20:20:17 -0400

    Renove conda-build dependency to see if it fixes things...

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 from setuptools import setup
-from conda_build.metadata import MetaData
-
-META = MetaData('devtools/conda-recipe')
 
 setup(
-    name=META.get_value('package/name'),
-    version=META.get_value('package/version'),
-    description=META.get_value('about/summary'),
-    url=META.get_value('about/home'),
+    name='ascommon',
+    version='0.0.1',
     author='Alexey Strokach',
     author_email='alex.strokach@utoronto.ca',
     packages=['ascommon'],
-    # setup_requires=['pytest-runner'],
-    # tests_require=['pytest'],
 )
