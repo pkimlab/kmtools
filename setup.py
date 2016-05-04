866b2461a31264985c1504fa36f18139fa9766a6
Alexey Strokach <alex.strokach@utoronto.ca>
2016-05-04 17:50:51 -0400

    "Release 0.0.4"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ascommon',
-    version='0.0.3',
+    version='0.0.4',
     author='Alexey Strokach',
     author_email='alex.strokach@utoronto.ca',
     packages=['ascommon'],
