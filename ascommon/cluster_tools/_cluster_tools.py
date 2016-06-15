c89f14fb3ca3b48905e461cdf20c24078e5035e2
ostrokach <ostrokach@gmail.com>
2016-06-15 15:30:36 -0400

    "Release 0.0.6"

--- ascommon/cluster_tools/_cluster_tools.py
+++ ascommon/cluster_tools/_cluster_tools.py
@@ -1,3 +1,42 @@
+import re
+
+
+def parse_connection_string(connection_string):
+    """Split `connection_string` into database parameters.
+
+    Examples
+    --------
+    >>> from pprint import pprint
+    >>> pprint(parse_connection_string('mysql://root:@localhost'))
+    {'db_name': '',
+     'db_type': 'mysql',
+     'host_ip': 'localhost',
+     'host_port': '',
+     'password': '',
+     'socket': '',
+     'username': 'root'}
+    >>> pprint(parse_connection_string('mysql://root:root_pass@192.168.0.1:3306/test'))
+    {'db_name': 'test',
+     'db_type': 'mysql',
+     'host_ip': '192.168.0.1',
+     'host_port': '3306',
+     'password': 'root_pass',
+     'socket': '',
+     'username': 'root'}
+    """
+    db_params = {}
+    (db_params['db_type'], db_params['username'], db_params['password'],
+     db_params['host_ip'], db_params['host_port'], db_params['db_name'],
+     db_params['socket']) = (
+        re.match(
+            '^(\w*)://(\w*):(\w*)@(localhost|[0-9\.]*)(|:[0-9]*)(|\/\w*)(|\?unix_socket=.*)$',
+            connection_string)
+        .groups()
+    )
+    db_params['host_port'] = db_params['host_port'].strip(':')
+    db_params['db_name'] = db_params['db_name'].strip('/')
+    db_params['socket'] = db_params['socket'].partition('?unix_socket=')[-1]
+    return db_params
 
 
 def iterate_parameters(parameter_grid, _params=None):
@@ -10,11 +49,9 @@ def iterate_parameters(parameter_grid, _params=None):
 
     Examples
     --------
-    >>> from collections import OrderedDict
-    >>> list(iterate_parameters(OrderedDict([('a', [1, 2]), ('b', [3, 4])]))) == [ \
-        {'a': 1, 'b': 3}, {'a': 2, 'b': 3}, {'a': 1, 'b': 4}, {'a': 2, 'b': 4} \
-    ]
-    True
+    >>> from pprint import pprint
+    >>> pprint(list(iterate_parameters({'a': [1, 2], 'b': [3, 4]})))
+    [{'a': 1, 'b': 3}, {'a': 2, 'b': 3}, {'a': 1, 'b': 4}, {'a': 2, 'b': 4}]
     """
     param_grid = parameter_grid.copy()
     # Don't modify dictionaries in-place
