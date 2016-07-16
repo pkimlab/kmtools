7d9bce39786df5e2584dfdcd59ee7c328b98ef5c
ostrokach <ostrokach@gmail.com>
2016-07-15 23:38:06 -0400

    "Release 0.0.7"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kmtools',
-    version='0.0.6',
+    version='0.0.7',
     author='kimlab.org',
     packages=find_packages(),
     package_data={
