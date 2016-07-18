96295f7084ec38e5c0646f8d30e289c5549e6466
ccorbi <carles.corbi@gmail.com>
2016-07-18 13:41:43 -0400

    solved import error

--- kmtools/sequence_tools/__init__.py
+++ kmtools/sequence_tools/__init__.py
@@ -2,6 +2,8 @@ from ._alignment_tools import *
 from ._blast import *
 from ._sequence_tools import *
 from .uniparc_xml_parser import UniParcXMLParser
+from .codons_info import *
+from .dna_util import *
 
 __all__ = [
     'uniparc_xml_parser',
