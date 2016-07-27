4d88d92850512dfd12f44d298905f8d7d13e10f5
ostrokach <ostrokach@gmail.com>
2016-07-27 16:04:20 -0400

    Added some common code from kimlaborg/datapkg and kimlaborg/elaspic.

--- tests/test_system_tools/test_system_tools.py
+++ tests/test_system_tools/test_system_tools.py
@@ -1,7 +1,12 @@
 import os.path as op
 from tempfile import TemporaryDirectory
 import pytest
+import tempfile
+import shutil
+import logging
 from kmtools import system_tools
+
+logger = logging.getLogger(__name__)
 
 
 # %%
@@ -44,7 +49,29 @@ class Test:
         assert op.isfile(output_file)
 
 
-# %%
-if __name__ == '__main__':
-    import pytest
-    pytest.main([__file__, '-svx'])
+class TestOpenExclusively:
+
+    @classmethod
+    def setup_class(cls):
+        cls.tempdir = tempfile.mkdtemp()
+        cls.tempfile = op.join(cls.tempdir, 'deleteme.txt')
+
+    def teardown_class(cls):
+        shutil.rmtree(cls.tempdir)
+
+    def _worker(self, x):
+        with system_tools.open_exclusively(self.tempfile) as ofh:
+            ofh.write('\t'.join(str(x) for i in range(10)) + '\n')
+
+    def test_open_exclusively(self):
+        logger.info("Running test_open_exclusively({})".format(self))
+        # Set up
+        NUMBER_OF_WRITES = 5000
+        # Run
+        from multiprocessing import Pool
+        with Pool(processes=36) as pool:
+            pool.map(self._worker, range(NUMBER_OF_WRITES))
+        # Results
+        with open(self.tempfile, 'r') as ofh:
+            data = ofh.readlines()
+        assert len(data) == NUMBER_OF_WRITES
