4f2c2cb7d39b3230ed86b6e7c64ab3bda343ccfd
ostrokach <ostrokach@gmail.com>
2016-08-29 21:21:58 -0400

    Unignore omnia_tools.

---
+++ kmtools/omnia_tools/__init__.py
@@ -0,0 +1,17 @@
+"""
+OMNIA Tools
+-----------
+
+# The following modules are required
+import simtk.openmm.app
+import simtk.openmm
+import simtk.unit
+
+import pdbfixer
+import parmed as pmd
+import mdtraj as mdpi
+import cpptraj
+
+
+from .openmm_tools import *
+"""
