07dec63ccfb004add49ce5e0f1a482b77db63cff
ostrokach <ostrokach@gmail.com>
2016-09-06 15:36:09 -0400

    "Release 0.0.14"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kmtools',
-    version='0.0.13',
+    version='0.0.14',
     author='kimlab.org',
     packages=['kmtools.' + x for x in find_packages('kmtools')],
     namespace_packages=['kmtools'],
