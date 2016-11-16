e39f510a4c0cf98accb9e13536e9e4e375fbfd0d
ostrokach <ostrokach@gmail.com>
2016-11-16 14:56:42 -0500

    Remove flake8 and pylint comments from __init__.py files.\ Ignore all __init__.py files altogether.

--- kmtools/df_tools/__init__.py
+++ kmtools/df_tools/__init__.py
@@ -1,3 +1,3 @@
-from ._pandas import *  # pylint: disable=W0401,E0401
-from ._data_analysis import *  # pylint: disable=W0401,E0401
-from ._loaders import *  # pylint: disable=W0401,E0401
+from ._pandas import *
+from ._data_analysis import *
+from ._loaders import *
