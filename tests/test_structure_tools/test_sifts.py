ba222c61f3bfd52ea6dde88cb5379d57e16b921b
ostrokach <ostrokach@gmail.com>
2017-05-13 21:45:55 -0400

    Fix how 'residx' column in sifts_df is generated
    and add more tests ;).

--- tests/test_structure_tools/test_sifts.py
+++ tests/test_structure_tools/test_sifts.py
@@ -1,7 +1,9 @@
+import hashlib
 import json
 import os.path as op
 from tempfile import TemporaryDirectory
 
+import numpy as np
 import pandas as pd
 import pytest
 from lxml import etree
@@ -9,29 +11,56 @@ from lxml import etree
 from kmtools import structure_tools
 
 
+@pytest.mark.parametrize(
+    'pdb_id, df_shape, df_md5_hash', [('1jrh', (442, 11), '8c45136a3db096221139ba0677d93adb')])
+def test_get_sifts_data(pdb_id, df_shape, df_md5_hash):
+    sifts_df = structure_tools.sifts.get_sifts_data(pdb_id)
+    assert sifts_df.shape == df_shape
+    assert hashlib.md5(sifts_df.to_msgpack()).hexdigest() == df_md5_hash
+
+
+@pytest.mark.parametrize(
+    'pdb_id, pdb_chains, pdb_mutations, results', [
+        (
+            '1jrh', 'L', 'L_I116W', {
+                'pdb_mutations_sifts': 'L_I116W',
+                'pfam_id_sifts': 'PF07654',
+                'uniprot_id_sifts': 'P01837',
+                'uniprot_mutations_sifts': 'I9W',
+            }),
+        (
+            '1jrh', 'I', 'I_T19L.I_E21K', {
+                'pdb_mutations_sifts': 'I_T19L.I_E21K',
+                'pfam_id_sifts': np.nan,
+                'uniprot_id_sifts': 'P15260',
+                'uniprot_mutations_sifts': 'T36L.E38K',
+            }),
+    ])
+def test_convert_pdb_mutation_to_uniprot(pdb_id, pdb_chains, pdb_mutations, results):
+    sifts_df = structure_tools.sifts.get_sifts_data('1jrh')
+    assert structure_tools.sifts.convert_pdb_mutations_to_uniprot(
+        pdb_id, pdb_chains, pdb_mutations, sifts_df=sifts_df) == results
+
+
 class TestPresent:
     """Test the case where the SIFTS xml file is already present in the cache directory."""
 
     @pytest.mark.parametrize('pdb_id', ['1arr', '3mbp', '1jrh'])
     def test_1(self, pdb_id):
-        structure_tools.sifts.CACHE_DIR = op.abspath(op.splitext(__file__)[0])
-        sifts_data = structure_tools.sifts.get_sifts_data(pdb_id)
+        cache_dir = op.abspath(op.splitext(__file__)[0])
+        sifts_data = structure_tools.sifts.get_sifts_data(pdb_id, cache_dir)
         assert isinstance(sifts_data, pd.DataFrame)
         assert not sifts_data.empty
 
     def test_2(self):
         pdb_id = '1dvf'
-        structure_tools.sifts.CACHE_DIR = op.abspath(op.splitext(__file__)[0])
-        sifts_data = structure_tools.sifts.get_sifts_data(pdb_id)
+        cache_dir = op.abspath(op.splitext(__file__)[0])
+        sifts_data = structure_tools.sifts.get_sifts_data(pdb_id, cache_dir)
         assert isinstance(sifts_data, pd.DataFrame)
         assert not sifts_data.empty
         sifts_data_subset = (
-            sifts_data[
-                (sifts_data['pdb_id'] == pdb_id) &
-                (sifts_data['pdb_chain'] == 'A') &
-                (sifts_data['resnum'] == '98')
-            ]
-        )
+            sifts_data[(sifts_data['pdb_id'] == pdb_id) & (sifts_data['pdb_chain'] == 'A') &
+                       (sifts_data['resnum'] == '98')])
         assert not sifts_data_subset.empty
         return sifts_data
 
@@ -42,27 +71,27 @@ class TestAbscent:
     @classmethod
     def setup_class(cls):
         cls.temp_dir = TemporaryDirectory()
-        structure_tools.sifts.CACHE_DIR = cls.temp_dir.name
 
     @classmethod
     def teardown_class(cls):
         cls.temp_dir.cleanup()
 
     def test_1(self):
-        sifts_data = structure_tools.sifts.get_sifts_data('1arr')
+        sifts_data = structure_tools.sifts.get_sifts_data('1arr', self.temp_dir.name)
         assert isinstance(sifts_data, pd.DataFrame)
         assert not sifts_data.empty
 
     def test_2(self):
-        sifts_data = structure_tools.sifts.get_sifts_data('3mbp')
+        sifts_data = structure_tools.sifts.get_sifts_data('3mbp', self.temp_dir.name)
         assert isinstance(sifts_data, pd.DataFrame)
         assert not sifts_data.empty
         del sifts_data
 
 
-@pytest.mark.parametrize("pdb_id,pdb_chains,pdb_mutations", [
-    ('2qja', 'C', 'T74A,L78M,G79K,L80Y'),
-])
+@pytest.mark.parametrize(
+    "pdb_id,pdb_chains,pdb_mutations", [
+        ('2qja', 'C', 'T74A,L78M,G79K,L80Y'),
+    ])
 def test_sifts_exception(pdb_id, pdb_chains, pdb_mutations):
     """Test the case where PDB AA and UniProt AA are different."""
     sifts_df = structure_tools.sifts.get_sifts_data(pdb_id)
