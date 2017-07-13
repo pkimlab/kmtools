9a70f0887c5fd40e1fa3b510f6cf36e14df25f29
ostrokach <ostrokach@gmail.com>
2017-07-13 17:31:12 -0400

    "Release 0.0.20"

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read_md(file):
 
 setup(
     name='kmtools',
-    version='0.0.20.dev0',
+    version='0.0.20',
     author='kimlab.org',
     author_email='alex.strokach@utoronto.ca',
     url="https://github.com/kimlaborg/kmtools",
