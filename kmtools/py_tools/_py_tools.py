567278cbec447db32c674982423e40e807206f2e
Alexey Strokach <ostrokach@gmail.com>
2017-08-30 00:52:35 -0400

    Rename 'unique' to 'uniquify' (functions should be verbs)

--- kmtools/py_tools/_py_tools.py
+++ kmtools/py_tools/_py_tools.py
@@ -81,12 +81,12 @@ def deserialize_object_from_string(data_hex_string):
     return obj
 
 
-def unique(l):
+def uniquify(l):
     """Return a list of unique elements of `l`, preserving order.
 
     Examples
     --------
-    >>> list(unique([1, 2, 3, 2, 1]))
+    >>> list(uniquify([1, 2, 3, 2, 1]))
     [1, 2, 3]
     """
     seen = set()
