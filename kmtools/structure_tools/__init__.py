d6d18ca52e54924c66929cab7cb2c00e9b93c15b
ostrokach <ostrokach@gmail.com>
2017-10-03 20:53:08 -0400

    Restructure .structure_tools modules

--- kmtools/structure_tools/__init__.py
+++ kmtools/structure_tools/__init__.py
@@ -10,12 +10,13 @@
    structure_parser
 """
 # flake8: noqa
-from .constants import *
-from .exc import *
-from .structure_parser import *
-from .elaspic_legacy import *
-
 __all__ = [
     'sifts',
 ]
 from . import *
+from .constants import *
+from .exc import *
+from .structure_parser import *
+from .elaspic_legacy import *
+from .optimizations import *
+from .viewers import *
