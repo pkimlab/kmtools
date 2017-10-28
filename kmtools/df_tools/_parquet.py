ba75667f5ff021cbb44f85e3b1997792872eb5a4
ostrokach <ostrokach@gmail.com>
2017-10-28 19:06:12 -0400

    Add WIP parquet support

--- kmtools/df_tools/_parquet.py
+++ kmtools/df_tools/_parquet.py
@@ -22,3 +22,21 @@ def _write_parquet_table(table_name, table_df, basedir=''):
     parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression='snappy')
     parquet_writer.write_table(table_pq)
     return parquet_file
+
+
+def convert_hdf5_to_parquet(h5_file, parquet_file, chunksize=100000):
+
+    stream = pd.read_csv(h5_file, chunksize=chunksize)
+
+    for i, chunk in enumerate(stream):
+        print("Chunk {}".format(i))
+
+        if i == 0:
+            # Infer schema and open parquet file on first chunk
+            parquet_schema = pa.Table.from_pandas(df=chunk).schema
+            parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression='snappy')
+
+        table = pa.Table.from_pandas(chunk, schema=parquet_schema)
+        parquet_writer.write_table(table)
+
+    parquet_writer.close()
