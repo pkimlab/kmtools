27bcd88aa068cefdc140420fbc60e76801f042e5
ostrokach <ostrokach@gmail.com>
2017-11-02 18:21:35 -0400

    Don't skip any residues when extracting the chain amino acid sequence

--- kmtools/structure_tools/sequence.py
+++ kmtools/structure_tools/sequence.py
@@ -1,30 +1,43 @@
 import hashlib
-import warnings
+import logging
 from typing import Optional
 
 import numpy as np
 
 from kmbio.PDB import Structure
-from kmtools.structure_tools import AAA_DICT
+from kmtools.structure_tools import AAA_DICT, RESIDUE_MAPPING_TO_CANONICAL
+
+logger = logging.getLogger(__name__)
 
 
 def extract_aa_sequence(structure: Structure, model_id: int, chain_id: str) -> Optional[str]:
     """Return amino acid sequence of all residues in chain.
 
-    Residues which cannot be assigned a single-character amino acid code are skipped.
+    Residues which cannot be assigned a single-character amino acid code represented as 'X'.
+
+    Args:
+        structure: Structure from which to extract the chain sequence.
+        model_id: ID of the model which contains the chain of interest.
+        chain_id: ID of the chain of interest.
+
+    Returns:
+        String of amino acids corresponding to the chain sequence.
+        The length of the returned string must equal to the number of residues in the chain.
+        Residues which cannot be mapped to a single-character amino acid are represented as 'X'.
     """
     aa_list = []
-    skipped_resnames = set()
     for residue in structure[model_id][chain_id]:
-        try:
-            aa_list.append(AAA_DICT[residue.resname])
-        except KeyError:
-            skipped_resnames.add(residue.resname)
-    if skipped_resnames:
-        warning = ("Skipped the following residues when generating chain sequence: {}"
-                   .format(skipped_resnames))
-        warnings.warn(warning)
+        if residue.resname in AAA_DICT:
+            residue_aa = AAA_DICT[residue.resname]
+        elif residue.resname in RESIDUE_MAPPING_TO_CANONICAL:
+            residue_aa = AAA_DICT[RESIDUE_MAPPING_TO_CANONICAL[residue.resname]]
+        else:
+            logger.warning("Cannot map residue %s to a single-character amino acid string.",
+                           residue.resname)
+            residue_aa = 'X'
+        aa_list.append(residue_aa)
     aa_string = ''.join(aa_list)
+    assert len(aa_string) == len(list(structure[model_id][chain_id].residues))
     return aa_string if aa_string else None
 
 
