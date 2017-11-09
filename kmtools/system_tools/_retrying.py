4d09f736f0788ff66c5e3fba39ec61e652af3ab0
ostrokach <ostrokach@gmail.com>
2017-11-08 21:59:33 -0500

    Add `retry_ssh`

--- kmtools/system_tools/_retrying.py
+++ kmtools/system_tools/_retrying.py
@@ -3,6 +3,7 @@ import logging
 import socket
 import subprocess
 
+import paramiko
 import sqlalchemy as sa
 from retrying import retry
 
@@ -19,31 +20,13 @@ def check_exception(exc, valid_exc):
     return to_retry
 
 
-def _retry_urlopen(fn):
-    """Retry downloading data from a url after a timeout.
-
-    Examples
-    --------
-    >>> import urllib.request
-    >>> with urllib.request.urlopen('http://google.ca') as ifh:
-    ...     data = _retry_urlopen(ifh.read)()
-    """
-    _check_exception = functools.partial(check_exception, valid_exc=socket.timeout)
-    wrapper = retry(
-        retry_on_exception=_check_exception,
-        wait_exponential_multiplier=1000,
-        wait_exponential_max=10000,
-        stop_max_attempt_number=5)
-    return wrapper(fn)
-
-
 def retry_database(fn):
     """Decorator to keep probing the database untill you succeed."""
     _check_exception = functools.partial(check_exception, valid_exc=sa.exc.OperationalError)
     wrapper = retry(
         retry_on_exception=_check_exception,
-        wait_exponential_multiplier=1000,
-        wait_exponential_max=60000,
+        wait_exponential_multiplier=1_000,
+        wait_exponential_max=60_000,
         stop_max_attempt_number=7)
     return wrapper(fn)
 
@@ -52,8 +35,8 @@ def retry_subprocess(fn):
     _check_exception = functools.partial(check_exception, valid_exc=subprocess.SubprocessError)
     wrapper = retry(
         retry_on_exception=_check_exception,
-        wait_exponential_multiplier=1000,
-        wait_exponential_max=60000,
+        wait_exponential_multiplier=1_000,
+        wait_exponential_max=60_000,
         stop_max_attempt_number=7)
     return wrapper(fn)
 
@@ -61,6 +44,33 @@ def retry_subprocess(fn):
 def retry_archive(fn):
     """Decorator to keep probing the database untill you succeed."""
     _check_exception = functools.partial(check_exception, valid_exc=system_tools.exc.ArchiveError)
+    wrapper = retry(retry_on_exception=_check_exception, wait_fixed=2000, stop_max_attempt_number=2)
+    return wrapper(fn)
+
+
+def retry_urlopen(fn):
+    """Retry downloading data from a url after a timeout.
+
+    Examples:
+        >>> import urllib.request
+        >>> with urllib.request.urlopen('http://google.ca') as ifh:
+        ...     data = retry_urlopen(ifh.read)()
+    """
+    _check_exception = functools.partial(check_exception, valid_exc=socket.timeout)
     wrapper = retry(
-        retry_on_exception=_check_exception, wait_fixed=2000, stop_max_attempt_number=2)
+        retry_on_exception=_check_exception,
+        wait_exponential_multiplier=1_000,
+        wait_exponential_max=10_000,
+        stop_max_attempt_number=5)
+    return wrapper(fn)
+
+
+def retry_ssh(fn):
+    """Retry doing something over an ssh connection."""
+    _check_exception = functools.partial(check_exception, valid_exc=paramiko.SSHException)
+    wrapper = retry(
+        retry_on_exception=_check_exception,
+        wait_exponential_multiplier=1_000,
+        wait_exponential_max=60_000,
+        stop_max_attempt_number=7)
     return wrapper(fn)
