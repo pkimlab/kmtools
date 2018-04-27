e9537a2d9c49d12e0b0c5989246e0a24ebb955b1
ostrokach <ostrokach@gmail.com>
2018-04-27 12:23:55 -0400

    add nproc option to `run_psipred`

--- kmtools/sequence_tools/psipred.py
+++ kmtools/sequence_tools/psipred.py
@@ -4,14 +4,30 @@ import shlex
 import subprocess
 from pathlib import Path
 from textwrap import dedent
+from typing import Mapping
 
 import Bio.SeqIO
 
 logger = logging.getLogger(__name__)
 
 
-def run_psipred(fasta_file: Path, hhblits_database: Path) -> Path:
-    """Predict secondary structure of sequence in `fasta_file`."""
+def run_psipred(fasta_file: Path, hhblits_database: Path, nproc: int = None) -> Path:
+    """Predict secondary structure of sequence in `fasta_file`.
+
+    Args:
+        fasta_file: File contianing the input sequence.
+        hhblits_database: Root folder of the ``hhblits`` database
+            (e.g. ``./uniprot20_2016_02``, not ``./uniprot20_2016_02/uniprot20_2016_02``).
+        nproc: Number of cores that should be used.
+
+    Returns:
+        Path of the file containing predicted secondary structure.
+    """
+    if nproc is not None:
+        env = {'OMP_NUM_THREADS': str(nproc)}
+    else:
+        env = {}
+
     hhblits_system_command = dedent(f"""\
     hhblits \
         -i {fasta_file} \
@@ -23,7 +39,7 @@ def run_psipred(fasta_file: Path, hhblits_database: Path) -> Path:
         -Z 100000 \
         -d {hhblits_database}/{hhblits_database.name}
     """)
-    _execute(hhblits_system_command)
+    _execute(hhblits_system_command, env)
 
     hhfilter_system_command = dedent(f"""\
     hhfilter \
@@ -33,7 +49,7 @@ def run_psipred(fasta_file: Path, hhblits_database: Path) -> Path:
         -neff 15 \
         -qsc -30
     """)
-    _execute(hhfilter_system_command)
+    _execute(hhfilter_system_command, env)
 
     addss_system_command = dedent(f"""\
     addss.pl \
@@ -41,12 +57,12 @@ def run_psipred(fasta_file: Path, hhblits_database: Path) -> Path:
         {fasta_file.with_suffix('.filt.withss.a3m')} \
         -a3m
     """)
-    _execute(addss_system_command)
+    _execute(addss_system_command, env)
 
     return fasta_file.with_suffix('.filt.withss.a3m')
 
 
-def read_psipred(psipred_file: Path) -> Path:
+def read_psipred(psipred_file: Path) -> str:
     """Read fasta file produced by `run_psipred`."""
     buf = io.StringIO()
     with psipred_file.open('rt') as fin:
@@ -59,14 +75,15 @@ def read_psipred(psipred_file: Path) -> Path:
     return str(psipred.seq)
 
 
-def _execute(system_command: str) -> None:
+def _execute(system_command: str, env: Mapping[str, str] = None) -> None:
+    env = env if env is not None else {}
     logger.debug(f"Running system command: '{system_command}'...")
     proc = subprocess.run(
         shlex.split(system_command),
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         universal_newlines=True,
-    )
+        env=env)
     if proc.returncode:
         logger.error(proc.stdout)
         logger.error(proc.stderr)
