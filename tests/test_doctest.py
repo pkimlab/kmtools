e03f3c03e4e2397b0ac4e2bb9acf0a7f88c3843c
ostrokach <ostrokach@gmail.com>
2018-07-24 12:30:22 -0400

    Set pandas settings to pass tests

--- tests/test_doctest.py
+++ tests/test_doctest.py
@@ -5,6 +5,7 @@ import os.path as op
 import tempfile
 
 import numpy as np
+import pandas as pd
 import pytest
 
 import kmtools
@@ -15,11 +16,14 @@ logger = logging.getLogger(__name__)
 DOCTEST_OPTIONFLAGS = (
     doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS | doctest.IGNORE_EXCEPTION_DETAIL)
 
-DOCTEST_EXTRAGLOBS = {'os': os, 'op': op, 'tempfile': tempfile, 'np': np}
+DOCTEST_EXTRAGLOBS = {'os': os, 'op': op, 'tempfile': tempfile, 'np': np, 'pd': pd}
 
 
 @pytest.mark.parametrize("module_name, module", py_tools.iter_submodules(kmtools))
 def test_doctest(module_name, module):
+    pd.options.display.width = 80
+    pd.options.display.max_rows = 100
+    pd.options.display.max_columns = 100
     failure_count, test_count = doctest.testmod(
         module, optionflags=DOCTEST_OPTIONFLAGS, extraglobs=DOCTEST_EXTRAGLOBS)
     assert failure_count == 0
