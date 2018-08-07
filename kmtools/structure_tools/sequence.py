d79feb9866a62a02922720eea01d40fabed7e3d6
Alexey Strokach <ostrokach@gmail.com>
2018-08-06 22:32:49 -0400

    WIP on standardize_mutation function

--- kmtools/structure_tools/sequence.py
+++ kmtools/structure_tools/sequence.py
@@ -1,11 +1,13 @@
 import hashlib
 import logging
+from pathlib import Path
 from typing import Optional
 
+import kmbio.PDB
 import numpy as np
 from kmbio.PDB import Structure
 
-from kmtools.structure_tools import AAA_DICT, RESIDUE_MAPPING_TO_CANONICAL
+from kmtools.structure_tools import A_DICT, AAA_DICT, PDB_RESIDUES, RESIDUE_MAPPING_TO_CANONICAL
 
 logger = logging.getLogger(__name__)
 
@@ -32,18 +34,19 @@ def extract_aa_sequence(structure: Structure, model_id: int, chain_id: str) -> O
         elif residue.resname in RESIDUE_MAPPING_TO_CANONICAL:
             residue_aa = AAA_DICT[RESIDUE_MAPPING_TO_CANONICAL[residue.resname]]
         else:
-            logger.debug("Cannot map residue %s to a single-character amino acid string.",
-                         residue.resname)
-            residue_aa = 'X'
+            logger.debug(
+                "Cannot map residue %s to a single-character amino acid string.", residue.resname
+            )
+            residue_aa = "X"
         aa_list.append(residue_aa)
-    aa_string = ''.join(aa_list)
+    aa_string = "".join(aa_list)
     assert len(aa_string) == len(list(structure[model_id][chain_id].residues))
     return aa_string if aa_string else None
 
 
 def extract_residue_sequence(structure: Structure, model_id: int, chain_id: str) -> Optional[str]:
     """Return comma-delimited residue sequence of all residues in chain."""
-    aa_string = ','.join(r.resname for r in structure[model_id][chain_id])
+    aa_string = ",".join(r.resname for r in structure[model_id][chain_id])
     return aa_string if aa_string else None
 
 
@@ -63,3 +66,37 @@ def hash_residue_pair(residue_pair) -> str:
     a = np.array(sorted(residue_pair))
     myhash.update(a)
     return myhash.hexdigest()
+
+
+def standardize_mutation(pdb_file: Path, pdb_mutation: str) -> str:
+    """Convert a residue RESNUM to a residue ID."""
+    structure = kmbio.PDB.load(pdb_file)
+
+    if "-" in pdb_mutation:
+        pdb_chain, mutation = pdb_mutation.split("-")
+    elif "_" in pdb_mutation:
+        pdb_chain, mutation = pdb_mutation.split("_")
+    else:
+        raise Exception(
+            "`pdb_mutation` must contain the chain and the mutation separated by '-' or '_'!"
+        )
+
+    try:
+        chain = structure[0][pdb_chain]
+    except KeyError:
+        raise Exception(f"Could not find chain '{pdb_chain}' in structure '{structure.id}'!")
+
+    found = False
+    residue_idx = 0
+    for residue in chain:
+        residue_id = f"{residue.id[1]}{residue.id[2]}"
+        if residue_id == mutation[1:-1]:
+            found = residue.resname == A_DICT[pdb_mutation[0]]
+            break
+        if residue.resname in PDB_RESIDUES:
+            residue_idx += 1
+
+    if found:
+        return f"{pdb_chain}-{pdb_mutation[0]}{residue_idx + 1}{pdb_mutation[-1]}"
+    else:
+        return None
