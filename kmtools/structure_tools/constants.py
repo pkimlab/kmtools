f092be6aa269ba9dcf16d9b1707336957a471f83
Alexey Strokach <ostrokach@gmail.com>
2018-08-06 21:34:56 -0400

    Add PDB_RESIDUES constant

--- kmtools/structure_tools/constants.py
+++ kmtools/structure_tools/constants.py
@@ -1,10 +1,12 @@
 import json
 import os.path as op
 import string
+from typing import List, Mapping
 
 DATA_DIR = op.join(op.dirname(op.abspath(__file__)), "data")
 
-A_DICT = {
+#: Mapping from 1-letter amino acid codes to 3-letter amino acid codes
+A_DICT: Mapping[str, str] = {
     "A": "ALA",
     "R": "ARG",
     "N": "ASN",
@@ -33,27 +35,30 @@ A_DICT = {
     "X": "XAA",
     "*": "TER",
 }
-AAA_DICT = {**{value: key for key, value in A_DICT.items()}, "UNK": "X"}
+
+#: Mapping from 3-letter amino acid codes to 1-letter amino acid codes
+AAA_DICT: Mapping[str, str] = {**{value: key for key, value in A_DICT.items()}, "UNK": "X"}
 
 METHYLATED_LYSINES = ["MLZ", "MLY", "M3L"]
 LYSINE_ATOMS = ["N", "CA", "CB", "CG", "CD", "CE", "NZ", "C", "O"]
 
+#: Heteroatoms that are very common in PDB structures
 COMMON_HETATMS = ["NH2", "HOH", "SO4"]
 
-CHAIN_IDS = list(string.ascii_uppercase + string.digits + string.ascii_lowercase)
+#: One- or two-letter identifies that should be assigned to new protein structures
+CHAIN_IDS: List[str] = list(string.ascii_uppercase + string.digits + string.ascii_lowercase)
 CHAIN_IDS += [(a + b) for a in CHAIN_IDS for b in CHAIN_IDS if a != b]
 
 # Standard accessibilities for a ALA-X-ALA tripeptide (obtained from NACCESS)
 with open(op.join(DATA_DIR, "standard_sasa.txt"), "rt") as fin:
     _standard_sasa = fin.read().strip()
-STANDARD_SASA_ALL = [
-    [l.strip() for l in line.split()] for line in _standard_sasa.split("\n")[1:]
-]
+STANDARD_SASA_ALL = [[l.strip() for l in line.split()] for line in _standard_sasa.split("\n")[1:]]
 STANDARD_SASA = {x[3]: float(x[4]) for x in STANDARD_SASA_ALL}
 
-# Map modified residue ids to canonical residue ids
+#: Map modified residue ids to canonical residue ids
+RESIDUE_MAPPING_TO_CANONICAL: Mapping[str, str]
 with open(op.join(DATA_DIR, "residue_mapping_to_canonical.json"), "rt") as fin:
     RESIDUE_MAPPING_TO_CANONICAL = json.load(fin)
-    RESIDUE_MAPPING_TO_CANONICAL.update(
-        {v: v for k, v in A_DICT.items() if k not in ["X", "*"]}
-    )
+
+#: All 3-letter codes that map to (modified) amino acids
+PDB_RESIDUES = set(AAA_DICT) | set(RESIDUE_MAPPING_TO_CANONICAL)
