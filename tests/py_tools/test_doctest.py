26ec8fdb67f542252dc12622ecf93791d89374fe
ostrokach <ostrokach@gmail.com>
2018-08-10 13:18:14 -0400

    Improve test_doctest.py scripts

--- tests/py_tools/test_doctest.py
+++ tests/py_tools/test_doctest.py
@@ -14,16 +14,19 @@ from kmtools import py_tools
 logger = logging.getLogger(__name__)
 
 DOCTEST_OPTIONFLAGS = (
-    doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS | doctest.IGNORE_EXCEPTION_DETAIL)
+    doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS | doctest.IGNORE_EXCEPTION_DETAIL
+)
 
-DOCTEST_EXTRAGLOBS = {'os': os, 'op': op, 'tempfile': tempfile, 'np': np, 'pd': pd}
+DOCTEST_EXTRAGLOBS = {"os": os, "op": op, "tempfile": tempfile, "np": np, "pd": pd}
 
 
 @pytest.mark.parametrize("module_name, module", py_tools.iter_submodules(kmtools.py_tools))
 def test_doctest(module_name, module):
-    pd.options.display.width = 80
-    pd.options.display.max_rows = 100
-    pd.options.display.max_columns = 100
-    failure_count, test_count = doctest.testmod(
-        module, optionflags=DOCTEST_OPTIONFLAGS, extraglobs=DOCTEST_EXTRAGLOBS)
+    with pd.option_context(
+        "display.width", 80, "display.max_rows", 100, "display.max_columns", 100
+    ):
+        with py_tools.disable_logging():
+            failure_count, test_count = doctest.testmod(
+                module, optionflags=DOCTEST_OPTIONFLAGS, extraglobs=DOCTEST_EXTRAGLOBS
+            )
     assert failure_count == 0
