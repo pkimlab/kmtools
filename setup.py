f9aafd43cb5f2104ea5c5fab7d1e5cced7c29477
Alexey Strokach <ostrokach@gmail.com>
2018-09-05 02:44:44 -0400

    Release 0.0.33

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ EXTENSIONS = [Extension("*", ["kmtools/sequence_tools/*.pyx"])]
 
 setup(
     name="kmtools",
-    version="0.0.32",
+    version="0.0.33",
     author="kimlab.org",
     author_email="alex.strokach@utoronto.ca",
     url="https://gitlab.com/kimlab/kmtools",
