0c792f9af774c4d7249fa531f92dfcd2b818b261
Alexey Strokach <ostrokach@gmail.com>
2018-09-12 07:33:04 -0400

    Fix test_hashes test (try 2)

--- tests/sequence_tools/test_hashes.py
+++ tests/sequence_tools/test_hashes.py
@@ -2,7 +2,7 @@ from hypothesis import given
 from hypothesis.strategies import text
 
 from kmtools.sequence_tools.hashes_fast import crc64
-from kmtools.sequence_tools.hashes_fast import crc64 as crc64_slow
+from kmtools.sequence_tools.hashes_slow import crc64 as crc64_slow
 
 
 @given(text())
