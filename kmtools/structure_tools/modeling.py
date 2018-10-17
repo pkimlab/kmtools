7d787b4c90d2db9f77e5dfb8edc9fe493354accb
ostrokach <ostrokach@gmail.com>
2018-10-17 00:52:38 -0400

    WIP with modeling to get it to work with pdb70 hh-suite database

--- kmtools/structure_tools/modeling.py
+++ kmtools/structure_tools/modeling.py
@@ -31,20 +31,60 @@ def write_pir_alignment(alignment: MultipleSeqAlignment, file: Path):
 
 
 def prepare_for_modeling(
-    structure: Structure, targets: List[DomainTarget]
+    structure: Structure, targets: List[DomainTarget], strict: bool = True
 ) -> Tuple[Structure, MultipleSeqAlignment]:
-    """Return a structure and an alignment that can be provided as input to Modeller."""
+    """Return a structure and an alignment that can be provided as input to Modeller.
+
+    Args:
+        strict: If ``False``, try hard to get things to work.
+            Useful, for example, if using hh-suite pdb70 database.
+    """
     template_structure = Structure(structure.id, [Model(0)])
     # Add amino acid chains
-    for chain_id, target in zip(CHAIN_IDS, targets):
+    for i, (chain_id, target) in enumerate(zip(CHAIN_IDS, targets)):
         residues = list(structure[target.model_id][target.chain_id].residues)
+        if target.template_start < 1:
+            offset = -target.template_start + 1
+            target = target._replace(
+                template_sequence="-" * offset + target.template_sequence[offset:], template_start=1
+            )
+            targets[i] = target
         if target.template_start is not None and target.template_end is not None:
-            residues = residues[target.template_start - 1 : target.template_end]
-        chain = Chain(chain_id, residues)
+            # start = False
+            # residues_subset = []
+            # for r in residues:
+            #     if r.id[1] == target.template_start:
+            #         start = True
+            #     if start:
+            #         residues_subset.append(r)
+            #     if r.id[1] == target.template_end:
+            #         break
+            residues_subset = residues[target.template_start - 1 : target.template_end]
+        chain = Chain(chain_id, residues_subset)
+        template_structure[0].add(chain)
         chain_sequence = structure_tools.get_chain_sequence(chain)
         chain_sequence_expected = target.template_sequence.replace("-", "")
-        assert chain_sequence == chain_sequence_expected, (chain_sequence, chain_sequence_expected)
-        template_structure[0].add(chain)
+        if chain_sequence != chain_sequence_expected:
+            if strict or chain_sequence >= chain_sequence_expected:
+                raise Exception(
+                    f"Chain sequence extracted from the structure does not match the provided "
+                    f"template sequence. ('{chain_sequence}' != '{chain_sequence_expected}'"
+                )
+            else:
+                template_alignment = []
+                chain_lag = 0
+                for j in range(len(target.target_sequence)):
+                    b = target.template_sequence[j]
+                    c = chain_sequence[j - chain_lag]
+                    if b == "-" or b != c:
+                        template_alignment.append("-")
+                        chain_lag += 1
+                    else:
+                        template_alignment.append(b)
+                target = target._replace(template_sequence="".join(template_alignment))
+                assert len(target.template_sequence) == j + 1
+                assert len(chain_sequence) == j + 1 - chain_lag
+                targets[i] = target
     # Extract chain sequences
     target_seq = "/".join([target.target_sequence for target in targets])
     template_seq = "/".join([target.template_sequence for target in targets])
