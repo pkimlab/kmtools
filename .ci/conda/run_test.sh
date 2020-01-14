e0045b2531d9a1e88e65bfb64446fd8833d15fda
ostrokach <ostrokach@gmail.com>
2020-01-14 17:01:25 -0500

    Do not sed coverage

--- .ci/conda/run_test.sh
+++ .ci/conda/run_test.sh
@@ -11,5 +11,5 @@ python -m pytest \
     --color=yes \
     "tests/${SUBPKG_NAME}"
 
-sed -i "s|${SP_DIR}||g" .coverage
+# sed -i "s|${SP_DIR}||g" .coverage
 mv .coverage "${PACKAGE_ROOT_DIR}/.coverage"
