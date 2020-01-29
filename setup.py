0bce8485f3c171b56abe142fe03a2704da821495
ostrokach <ostrokach@gmail.com>
2020-01-29 12:15:20 -0500

    Release 0.1.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ EXTENSIONS = [Extension("*", ["kmtools/sequence_tools/*.pyx"])]
 
 setup(
     name="kmtools",
-    version="0.1.2",
+    version="0.1.3",
     author="kimlab.org",
     author_email="alex.strokach@utoronto.ca",
     url="https://gitlab.com/kimlab/kmtools",
