019e3248c6bac429165df6d0f92cab81a48d5f45
ostrokach <ostrokach@gmail.com>
2020-04-01 13:18:01 -0400

    Add add_cbetas function

--- kmtools/structure_tools/fixes.py
+++ kmtools/structure_tools/fixes.py
@@ -3,7 +3,11 @@ import logging
 import shlex
 import subprocess
 from pathlib import Path
-from typing import IO, Union
+from typing import IO, Optional, Union
+
+import numpy as np
+import pandas as pd
+from scipy import stats
 
 logger = logging.getLogger(__name__)
 
@@ -56,3 +60,63 @@ def _protonate_with_openmm(input_file: IO, output_file: IO) -> None:
     modeller = app.Modeller(pdb.getTopology(), pdb.getPositions())
     modeller.addHydrogens(pH=7.0)
     app.PDBFile.writeFile(modeller.topology, modeller.positions, output_file, keepIds=True)
+
+
+def add_cbetas(
+    structure_df: pd.DataFrame,
+    internal_coords: np.ndarray,
+    use_median: bool = False,
+    random_state: Optional[int] = None,
+) -> pd.DataFrame:
+    """Add C-beta atoms to every residue (including glycine) in the provided dataframe.
+
+    Useful in cases where we want to e.g. calculate the distance between the C-beta atom
+    of every residue in the structure.
+    """
+    assert (
+        structure_df["residue_idx"].sort_values().values == structure_df["residue_idx"].values
+    ).all()
+    assert (
+        structure_df["atom_idx"].sort_values().unique() == structure_df["atom_idx"].values
+    ).all()
+    assert structure_df["residue_idx"].unique().shape[0] == internal_coords.shape[0]
+
+    cb_vector_stats = [
+        (0.917_886_557_702_201_5, 0.038_527_049_007_855_82),
+        (-1.212_703_163_460_762, 0.029_876_758_154_174_805),
+        (-0.028_442_792_464_114_74, 0.023_039_163_265_260_055),
+    ]
+    cb_vector_rvs = [stats.laplace(*stat) for stat in cb_vector_stats]
+
+    cbeta_dfs = []
+    for (residue_idx, residue_df), residue_internal_coords in zip(
+        structure_df.groupby("residue_idx"), internal_coords
+    ):
+        if "CB" in residue_df["atom_name"].values:
+            continue
+        ca_row = residue_df[residue_df["atom_name"] == "CA"].iloc[:1].copy()
+        ca_coords = ca_row.iloc[0][["atom_x", "atom_y", "atom_z"]].values
+        if use_median:
+            cb_vector_internal = np.hstack([stat[0] for stat in cb_vector_stats])
+        else:
+            cb_vector_internal = np.hstack(
+                [rv.rvs(random_state=random_state) for rv in cb_vector_rvs]
+            )
+        cb_vector_pred = cb_vector_internal @ residue_internal_coords
+        cb_coords_pred = ca_coords - cb_vector_pred
+        ca_row["atom_name"] = "CB"
+        ca_row["atom_fullname"] = " CB "
+        ca_row["atom_x"], ca_row["atom_y"], ca_row["atom_z"] = cb_coords_pred
+        cbeta_dfs.append(ca_row)
+
+    if cbeta_dfs:
+        structure_df = pd.concat([structure_df] + cbeta_dfs, ignore_index=True).sort_values(
+            ["atom_idx", "atom_name"]
+        )
+        structure_df["atom_idx"] = np.arange(len(structure_df))
+        structure_df.index = np.arange(len(structure_df))
+        assert (
+            structure_df["residue_idx"].sort_values().values == structure_df["residue_idx"].values
+        ).all()
+
+    return structure_df
--- tests/structure_tools/test_fixes.py
+++ tests/structure_tools/test_fixes.py
@@ -3,7 +3,17 @@ import tempfile
 from pathlib import Path
 from unittest.mock import patch
 
-from kmtools.structure_tools.fixes import _protonate_with_openmm, _protonate_with_reduce, protonate
+import numpy as np
+import pytest
+from kmbio import PDB
+
+from kmtools.structure_tools.fixes import (
+    _protonate_with_openmm,
+    _protonate_with_reduce,
+    add_cbetas,
+    protonate,
+)
+from kmtools.structure_tools.protein_structure_analysis import get_internal_coords
 
 
 @patch("kmtools.structure_tools.fixes._protonate_with_reduce")
@@ -50,3 +60,49 @@ def test__protonate_with_reduce():
     output_file.seek(0)
     lines = list(output_file.read().strip().split("\n"))
     assert len(lines) == 155
+
+
+@pytest.mark.parametrize(
+    "structure_name, use_median",
+    [
+        (structure_name, use_median)
+        for structure_name in ["AAA.pdb", "ADNA.pdb", "AEA.pdb"]
+        for use_median in [True, False]
+    ],
+)
+def test_add_cbetas(structure_name, use_median):
+    structure_file = Path(__file__).parent.joinpath("structures", structure_name)
+    structure = PDB.load(structure_file)
+    structure_df = structure.to_dataframe()
+    internal_coords = get_internal_coords(structure_df)
+    structure_df_nocbetas = structure_df[structure_df["atom_name"] != "CB"]
+    structure_df_wcbetas = add_cbetas(
+        structure_df_nocbetas, internal_coords, use_median=False, random_state=0
+    )
+
+    def reorder_cbetas(structure_df):
+        structure_df_ = structure_df.copy()
+        structure_df_.loc[structure_df_["atom_name"] == "CB", "atom_idx"] = structure_df_.loc[
+            structure_df_["atom_name"] == "CA", "atom_idx"
+        ].values
+        structure_df_.sort_values(["atom_idx", "atom_name"], inplace=True)
+        structure_df_["atom_idx"] = structure_df["atom_idx"].values
+        structure_df_.index = structure_df.index.values
+        return structure_df_
+
+    structure_df_ = reorder_cbetas(structure_df)
+    assert structure_df_.columns.equals(structure_df_wcbetas.columns)
+
+    coord_columns = ["atom_x", "atom_y", "atom_z"]
+    assert np.allclose(
+        structure_df_[coord_columns].values,
+        structure_df_wcbetas[coord_columns].values,
+        rtol=0.1,
+        atol=0.05,
+    )
+
+    columns_to_skip = ["atom_serial_number"]
+    noncoord_columns = [
+        c for c in structure_df_.columns if c not in coord_columns and c not in columns_to_skip
+    ]
+    assert structure_df_[noncoord_columns].equals(structure_df_wcbetas[noncoord_columns])
